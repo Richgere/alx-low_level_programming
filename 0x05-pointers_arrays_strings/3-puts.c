4648cba73dfab05130881c68f3fd48f04f8b93f7
Richgere <99529144+Richgere@users.noreply.github.com>
2022-03-23 04:27:43 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,15 +3,10 @@
   * _puts - prints a string followed by a new line 
   * @str: input 
   */ 
- void _puts(char *str) 
- { 
- 	int i = 0; 
- 
- 
- 	while (str[i] != '\0') 
- 	{ 
-		_putchar(str[i]); 
-		i++; 
-  } 
-   _putchar('\n'); 
- } 
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
