8f0a205365d708c6d48c739f5cc7f501e6c6b0f3
Richgere <99529144+Richgere@users.noreply.github.com>
2022-03-25 00:01:04 +0300

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
- /*
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+  /*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
@@ -10,14 +18,6 @@
    */
   ;
   /* ...so that this prints 98\n */
-int main(void)
-{
-  int n;
-  int a[5];
-  int *p;
-
-  a[2] = 1024;
-  p = &n;
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
