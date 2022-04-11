8699d9b1f4c31fde5e18dc5328af5efdb87fe082
Richgere <99529144+Richgere@users.noreply.github.com>
2022-04-11 20:22:36 +0300

    Create 101-preprocessor_abuse.c

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define MSG "Hello, Holberton"
+
+/**
+ * main - prints a message.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", MSG);
+	return (0);
+}
+© 2022 GitHub, Inc.
+Terms
